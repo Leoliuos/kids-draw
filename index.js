3e033dc130bc5136855c6709a86faac03bf3ccba
LeoLiuos <leo.max.lipasti@gmail.com>
2019-05-11 15:01:16 +0200

    for heroku set up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
-    origins: "localhost:8080 onceivesetupadreesforherokuhere.herokuapp.com:*"
+    origins: "localhost:8080 kids-draw.herokuapp.com:*"
 });
 const compression = require("compression");
 
