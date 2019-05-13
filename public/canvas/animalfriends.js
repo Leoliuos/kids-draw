84e869a7eeaeb9147ad840095d1d1707025539d4
LeoLiuos <leo.max.lipasti@gmail.com>
2019-05-13 16:03:01 +0200

    visiting animals, now with just delay, perhaps something more later

--- public/canvas/animalfriends.js
+++ public/canvas/animalfriends.js
@@ -1,101 +1,131 @@
-const theme = "forest";
-console.log(theme);
+(function() {
+    const theme = "forest";
+    console.log(theme);
 
-const bg = document.getElementById("bg");
-bg.src = "/images/background_" + theme + ".jpg";
+    const bg = document.getElementById("bg");
+    bg.src = "/images/background_" + theme + ".jpg";
 
-const bottomleft = document.getElementById("bottomleft");
-const bottomleftalt = document.getElementById("bottomleft_alt");
-const bottom = document.getElementById("bottom");
-const bottomalt = document.getElementById("bottom_alt");
-const bottomright = document.getElementById("bottomright");
-const bottomrightalt = document.getElementById("bottomright_alt");
-const topright = document.getElementById("topright");
-const walker = document.getElementById("walker");
-const walkeralt = document.getElementById("walker_alt");
-const fly = document.getElementById("fly");
+    const bottomleft = document.getElementById("bottomleft");
+    const bottomleftalt = document.getElementById("bottomleft_alt");
+    const bottom = document.getElementById("bottom");
+    const bottomalt = document.getElementById("bottom_alt");
+    const bottomright = document.getElementById("bottomright");
+    const bottomrightalt = document.getElementById("bottomright_alt");
+    const topright = document.getElementById("topright");
+    const walker = document.getElementById("walker");
+    const walkeralt = document.getElementById("walker_alt");
+    const fly = document.getElementById("fly");
 
-// Image paths set up
+    const characters = [
+        bottomleft,
+        bottomleftalt,
+        bottom,
+        bottomalt,
+        bottomright,
+        bottomrightalt,
+        topright,
+        walker,
+        walkeralt,
+        fly
+    ];
 
-const imagepath = "/images/animals/" + theme + "_";
-const format = ".png";
+    // Image paths set up
 
-bottomleft.src = imagepath + bottomleft.id + format;
-bottomleftalt.src = imagepath + bottomleftalt.id + format;
-bottom.src = imagepath + bottom.id + format;
-bottomalt.src = imagepath + bottomalt.id + format;
-bottomright.src = imagepath + bottomright.id + format;
-bottomrightalt.src = imagepath + bottomrightalt.id + format;
-topright.src = imagepath + topright.id + format;
-walker.src = imagepath + walker.id + format;
-walkeralt.src = imagepath + walkeralt.id + format;
-fly.src = imagepath + fly.id + format;
+    const imagepath = "/images/animals/" + theme + "_";
+    const format = ".png";
 
-// Image sizing, original pixel sizes * adjusted scale
-const circusscale = 1.5;
-const imgsizes = {
-    forest: {
-        bottomleft: [2144, 3219],
-        bottomleftalt: [1957, 1387],
-        bottom: [1206, 2547],
-        bottomalt: [990, 1471],
-        bottomright: [2193, 1738],
-        bottomrightalt: [1114, 1362],
-        topright: [1479, 2224],
-        walker: [552, 513],
-        walkeralt: [947, 746],
-        fly: [467, 379]
-    },
-    autumn: {
-        bottomleft: [1649, 1569],
-        bottomleftalt: [1327, 1238],
-        bottom: [812, 797],
-        bottomalt: [749, 1228],
-        bottomright: [1360, 1209],
-        bottomrightalt: [1071, 994],
-        topright: [797, 1336],
-        walker: [811, 641],
-        walkeralt: [868, 1193],
-        fly: [610, 755]
-    },
-    circus: {
-        bottomleft: [1238 * circusscale, 1615 * circusscale],
-        bottomleftalt: [1890, 2247],
-        bottom: [831 * circusscale, 1269 * circusscale],
-        bottomalt: [993 * circusscale, 1482 * circusscale],
-        bottomright: [947 * circusscale, 1532 * circusscale],
-        bottomrightalt: [1944, 2065],
-        topright: [1289 * circusscale, 987 * circusscale],
-        walker: [487, 569],
-        walkeralt: [596, 505],
-        fly: [518, 425]
+    for (var i = 0; i < characters.length; i++) {
+        characters[i].src = imagepath + characters[i].id + format;
     }
-};
 
-// Image sizing / 10
+    // Image sizing, original pixel sizes * adjusted scale
+    const circusscale = 1.5;
+    const imgsizes = {
+        forest: {
+            bottomleft: [2144, 3219],
+            bottomleftalt: [1957, 1387],
+            bottom: [1206, 2547],
+            bottomalt: [990, 1471],
+            bottomright: [2193, 1738],
+            bottomrightalt: [1114, 1362],
+            topright: [1479, 2224],
+            walker: [552, 513],
+            walkeralt: [947, 746],
+            fly: [467, 379]
+        },
+        autumn: {
+            bottomleft: [1649, 1569],
+            bottomleftalt: [1327, 1238],
+            bottom: [812, 797],
+            bottomalt: [749, 1228],
+            bottomright: [1360, 1209],
+            bottomrightalt: [1071, 994],
+            topright: [797, 1336],
+            walker: [811, 641],
+            walkeralt: [868, 1193],
+            fly: [610, 755]
+        },
+        circus: {
+            bottomleft: [1238 * circusscale, 1615 * circusscale],
+            bottomleftalt: [1890, 2247],
+            bottom: [831 * circusscale, 1269 * circusscale],
+            bottomalt: [993 * circusscale, 1482 * circusscale],
+            bottomright: [947 * circusscale, 1532 * circusscale],
+            bottomrightalt: [1944, 2065],
+            topright: [1289 * circusscale, 987 * circusscale],
+            walker: [487, 569],
+            walkeralt: [596, 505],
+            fly: [518, 425]
+        }
+    };
 
-bottomleft.width = imgsizes[theme].bottomleft[0] / 10;
-bottomleftalt.width = imgsizes[theme].bottomleftalt[0] / 10;
-bottom.width = imgsizes[theme].bottom[0] / 10;
-bottomalt.width = imgsizes[theme].bottomalt[0] / 10;
-bottomright.width = imgsizes[theme].bottomright[0] / 10;
-bottomrightalt.width = imgsizes[theme].bottomrightalt[0] / 10;
-topright.width = imgsizes[theme].topright[0] / 10;
-walker.width = imgsizes[theme].walker[0] / 10;
-walkeralt.width = imgsizes[theme].walkeralt[0] / 10;
-fly.width = imgsizes[theme].fly[0] / 10;
+    // Image sizing / 10
 
-// audio
+    bottomleft.width = imgsizes[theme].bottomleft[0] / 10;
+    bottomleftalt.width = imgsizes[theme].bottomleftalt[0] / 10;
+    bottom.width = imgsizes[theme].bottom[0] / 10;
+    bottomalt.width = imgsizes[theme].bottomalt[0] / 10;
+    bottomright.width = imgsizes[theme].bottomright[0] / 10;
+    bottomrightalt.width = imgsizes[theme].bottomrightalt[0] / 10;
+    topright.width = imgsizes[theme].topright[0] / 10;
+    walker.width = imgsizes[theme].walker[0] / 10;
+    walkeralt.width = imgsizes[theme].walkeralt[0] / 10;
+    fly.width = imgsizes[theme].fly[0] / 10;
 
-var music = document.getElementById("music");
+    // audio
 
-if (theme != "circus") {
-    music.src = "/sounds/music_david_gwyn_jones_singing_away_instrumental.mp3";
-} else {
-    music.src = "/sounds/audio_hero_Merry-Go-Round_SIPML_Q-0145.mp3";
-}
-window.addEventListener("click", function() {
-    if (music.paused) {
-        music.play();
+    var music = document.getElementById("music");
+
+    if (theme != "circus") {
+        music.src =
+            "/sounds/music_david_gwyn_jones_singing_away_instrumental.mp3";
+    } else {
+        music.src = "/sounds/audio_hero_Merry-Go-Round_SIPML_Q-0145.mp3";
     }
-});
+
+    window.addEventListener("click", function musicPlay() {
+        if (music.paused) {
+            music.play();
+            window.removeEventListener("click", musicPlay);
+        }
+    });
+
+    var nexttimeout = setTimeout(movetoScreen, 19000);
+    var i = 0;
+    function movetoScreen() {
+        characters[i].classList.add("destination");
+        nexttimeout = setTimeout(returnPosition, 3000);
+        i++;
+        if (i > characters.length - 1) {
+            i = 0;
+        }
+    }
+
+    function returnPosition() {
+        for (var i = 0; i < characters.length; i++) {
+            characters[i].classList.remove("destination");
+        }
+        clearTimeout(nexttimeout);
+        nexttimeout = setTimeout(movetoScreen, 19000);
+    }
+})();
--- public/canvas/canvas_v2.js
+++ public/canvas/canvas_v2.js
@@ -1,213 +1,217 @@
-console.log("ready to go");
+(function() {
+    console.log("ready to go");
 
-const c = document.getElementById("drawboard");
+    const c = document.getElementById("drawboard");
 
-const ctx = c.getContext("2d");
-ctx.canvas.width = Math.min(document.body.clientWidth * 0.8, 1754);
-ctx.canvas.height = Math.min(document.body.clientWidth * 0.8 * 0.7, 1240);
-c.style.marginLeft = document.body.clientWidth * 0.1 + "px";
-c.style.marginTop = document.body.clientWidth * 0.025 + "px";
+    const ctx = c.getContext("2d");
+    ctx.canvas.width = Math.min(document.body.clientWidth * 0.8, 1754);
+    ctx.canvas.height = Math.min(document.body.clientWidth * 0.8 * 0.7, 1240);
+    c.style.marginLeft = document.body.clientWidth * 0.1 + "px";
+    c.style.marginTop = document.body.clientWidth * 0.025 + "px";
 
-let toggle = false;
+    let toggle = false;
 
-let coordlastframe = [0, 0];
+    let coordlastframe = [0, 0];
 
-let lastbigwidth = 0;
-let lastsmallwidth = 0;
-// Kids Draw canvas by Leo Lipasti
+    let lastbigwidth = 0;
+    let lastsmallwidth = 0;
+    // Kids Draw canvas by Leo Lipasti
 
-// these are shades from royalblue to white for ballpoint pen
-const ballpointshadesblue = [
-    "#4169e1",
-    "#5276e3",
-    "#6384e6",
-    "#7491e9",
-    "#869feb",
-    "#97adee",
-    "#b9c8f4",
-    "#dce3f9"
-];
-
-// these are shades for black ballpoint pen
-const ballpointshadesblack = [
-    "#000000",
-    "#181818",
-    "#303030",
-    "#404040",
-    "#505050",
-    "#606060",
-    "#888888",
-    "#B0B0B0"
-];
-
-// these are shades for red ballpoint pen
-const ballpointshadesred = [
-    "#ff2819",
-    "#f22a1d",
-    "#f23326",
-    "#f2453a",
-    "#f25248",
-    "#ef5f56",
-    "#ed7068",
-    "#ef837c"
-];
-// these are shades for green ballpoint pen
-const ballpointshadesgreen = [
-    "#00d60d",
-    "#06d813",
-    "#0ddd1a",
-    "#16e223",
-    "#24e531",
-    "#34e540",
-    "#4ce857",
-    "#61ed6b"
-];
-
-let ballpointshades = ballpointshadesgreen;
-
-var mousecoordMoved = [0, 0];
-
-c.addEventListener("mousedown", function(event) {
-    touchFunction(event, event);
-});
-c.addEventListener("touchstart", function(event) {
-    // preventing mouse event
-    event.preventDefault();
-    touchFunction(event.changedTouches[0], event);
-});
-
-var touchFunction = function(touchevent, event) {
-    //var event = ;
-    mousecoordMoved = [
-        touchevent.clientX - event.currentTarget.offsetLeft,
-        touchevent.clientY - event.currentTarget.offsetTop
+    // these are shades from royalblue to white for ballpoint pen
+    const ballpointshadesblue = [
+        "#4169e1",
+        "#5276e3",
+        "#6384e6",
+        "#7491e9",
+        "#869feb",
+        "#97adee",
+        "#b9c8f4",
+        "#dce3f9"
     ];
-    toggle = true;
-    // clamping the pressure value to half to reduce too think lines in the middle :
-    penpressure = 6;
-};
 
-c.addEventListener("mouseup", function(event) {
-    releaseFunction(event, event);
-});
-c.addEventListener("touchend", function(event) {
-    // preventing mouse event
-    event.preventDefault();
-    releaseFunction(event.changedTouches[0], event);
-});
+    // these are shades for black ballpoint pen
+    const ballpointshadesblack = [
+        "#000000",
+        "#181818",
+        "#303030",
+        "#404040",
+        "#505050",
+        "#606060",
+        "#888888",
+        "#B0B0B0"
+    ];
 
-var releaseFunction = function(touchevent, event) {
-    if (
-        Math.abs(
-            Math.hypot(
-                Math.abs(
-                    touchevent.clientX -
-                        event.currentTarget.offsetLeft -
-                        mousecoordMoved[0]
-                ),
-                Math.abs(
-                    touchevent.clientY -
-                        event.currentTarget.offsetTop -
-                        mousecoordMoved[1]
-                )
-            ) < 0.1
-        )
-    ) {
-        // mouse did not move much, therefore creating a dot .
-        ctx.beginPath();
-        ctx.moveTo(mousecoordMoved[0], mousecoordMoved[1]);
-        ctx.lineTo(mousecoordMoved[0] + 1, mousecoordMoved[1] + 1);
-        ctx.strokeStyle = ballpointshades[0];
-        ctx.lineCap = "round";
-        ctx.lineWidth = 3.5;
-        ctx.stroke();
-    }
-    toggle = false;
-    coordlastframe = [0, 0];
-};
+    // these are shades for red ballpoint pen
+    const ballpointshadesred = [
+        "#ff2819",
+        "#f22a1d",
+        "#f23326",
+        "#f2453a",
+        "#f25248",
+        "#ef5f56",
+        "#ed7068",
+        "#ef837c"
+    ];
+    // these are shades for green ballpoint pen
+    const ballpointshadesgreen = [
+        "#00d60d",
+        "#06d813",
+        "#0ddd1a",
+        "#16e223",
+        "#24e531",
+        "#34e540",
+        "#4ce857",
+        "#61ed6b"
+    ];
 
-c.addEventListener("mouseleave", function(event) {
-    toggle = false;
-    coordlastframe = [0, 0];
-});
-c.addEventListener("touchcancel", function(event) {
-    // preventing mouse event
-    event.preventDefault();
-    toggle = false;
-    coordlastframe = [0, 0];
-});
+    let ballpointshades = ballpointshadesgreen;
 
-c.addEventListener("mousemove", function(event) {
-    moveFunction(event, event);
-});
-c.addEventListener("touchmove", function(event) {
-    // preventing mouse event
-    event.preventDefault();
-    moveFunction(event.changedTouches[0], event);
-});
+    var mousecoordMoved = [0, 0];
 
-var moveFunction = function(touchevent, event) {
-    if (toggle) {
-        mousecoord = [
+    c.addEventListener("mousedown", function(event) {
+        touchFunction(event, event);
+    });
+    c.addEventListener("touchstart", function(event) {
+        // preventing mouse event
+        event.preventDefault();
+        touchFunction(event.changedTouches[0], event);
+    });
+
+    var touchFunction = function(touchevent, event) {
+        //var event = ;
+        mousecoordMoved = [
             touchevent.clientX - event.currentTarget.offsetLeft,
             touchevent.clientY - event.currentTarget.offsetTop
         ];
-        ctx.beginPath();
-        if (coordlastframe[0] === 0 && coordlastframe[1] === 0) {
-            ctx.moveTo(mousecoord[0], mousecoord[1]);
-        } else {
-            ctx.moveTo(coordlastframe[0], coordlastframe[1]);
+        toggle = true;
+        // clamping the pressure value to half to reduce too think lines in the middle :
+        penpressure = 6;
+    };
+
+    c.addEventListener("mouseup", function(event) {
+        releaseFunction(event, event);
+    });
+    c.addEventListener("touchend", function(event) {
+        // preventing mouse event
+        event.preventDefault();
+        releaseFunction(event.changedTouches[0], event);
+    });
+
+    var releaseFunction = function(touchevent, event) {
+        if (
+            Math.abs(
+                Math.hypot(
+                    Math.abs(
+                        touchevent.clientX -
+                            event.currentTarget.offsetLeft -
+                            mousecoordMoved[0]
+                    ),
+                    Math.abs(
+                        touchevent.clientY -
+                            event.currentTarget.offsetTop -
+                            mousecoordMoved[1]
+                    )
+                ) < 0.1
+            )
+        ) {
+            // mouse did not move much, therefore creating a dot .
+            ctx.beginPath();
+            ctx.moveTo(mousecoordMoved[0], mousecoordMoved[1]);
+            ctx.lineTo(mousecoordMoved[0] + 1, mousecoordMoved[1] + 1);
+            ctx.strokeStyle = ballpointshades[0];
+            ctx.lineCap = "round";
+            ctx.lineWidth = 3.5;
+            ctx.stroke();
         }
-        ctx.lineTo(mousecoord[0], mousecoord[1]);
-
-        // START OF // LINE WIDTHS // LINE WIDTHS // LINE WIDTHS
-        // pen pressure drops or ink runs down, either way, pressure falls each frame, Min value 1.5
-        penpressure = Math.max(penpressure - 0.05, 1.5);
-        // mouse movement vector length per frame. Using it to determine line size. Faster = thinner line
-        let vectorlen = Math.hypot(
-            Math.abs(mousecoord[0] - coordlastframe[0]),
-            Math.abs(mousecoord[1] - coordlastframe[1])
-        );
-        // 1.4142... is just a number that came up often while moving really slow with my mouse
-        // seemed like a good zero state in this case but could be anything, just a value to clamp lowest values to
-        let bigwidth =
-            3 - Math.min(Math.max(vectorlen - 1.4142135623730951, 0), 6) / 2;
-        // smooths size change per frame 1/4 on the new pressure & penpressure :
-        bigwidth = Math.min((bigwidth + lastbigwidth * 3) / 4, penpressure);
-        let smallwidth =
-            1.75 - Math.min(Math.max(vectorlen - 1.4142135623730951, 0), 2) / 2;
-        // smooths size change per frame 1/3 on the new pressure & penpressure :
-        smallwidth = Math.min(
-            (smallwidth + lastsmallwidth * 2) / 3,
-            penpressure
-        );
-        lastbigwidth = bigwidth;
-        lastsmallwidth = smallwidth;
-        // END OF // LINE WIDTHS // LINE WIDTHS // LINE WIDTHS
-
-        // Royal blue shades by vector length
-        let shadeselect = Math.min(
-            Math.round(vectorlen - 2),
-            ballpointshades.length - 1
-        );
-
-        ctx.strokeStyle = ballpointshades[shadeselect];
-        ctx.lineCap = "round";
-        ctx.lineWidth = bigwidth;
-        ctx.stroke();
-        ctx.beginPath();
-        if (coordlastframe[0] === 0 && coordlastframe[1] === 0) {
-            ctx.moveTo(mousecoord[0], mousecoord[1]);
-        } else {
-            ctx.moveTo(coordlastframe[0], coordlastframe[1]);
-        }
-        ctx.lineTo(mousecoord[0], mousecoord[1]);
-        ctx.lineWidth = smallwidth;
-        ctx.strokeStyle = ballpointshades[Math.max(shadeselect - 2, 0)];
-        ctx.lineCap = "round";
-        ctx.stroke();
-        coordlastframe = [mousecoord[0], mousecoord[1]];
-    } else {
         toggle = false;
-    }
-};
+        coordlastframe = [0, 0];
+    };
+
+    c.addEventListener("mouseleave", function(event) {
+        toggle = false;
+        coordlastframe = [0, 0];
+    });
+    c.addEventListener("touchcancel", function(event) {
+        // preventing mouse event
+        event.preventDefault();
+        toggle = false;
+        coordlastframe = [0, 0];
+    });
+
+    c.addEventListener("mousemove", function(event) {
+        moveFunction(event, event);
+    });
+    c.addEventListener("touchmove", function(event) {
+        // preventing mouse event
+        event.preventDefault();
+        moveFunction(event.changedTouches[0], event);
+    });
+
+    var moveFunction = function(touchevent, event) {
+        if (toggle) {
+            mousecoord = [
+                touchevent.clientX - event.currentTarget.offsetLeft,
+                touchevent.clientY - event.currentTarget.offsetTop
+            ];
+            ctx.beginPath();
+            if (coordlastframe[0] === 0 && coordlastframe[1] === 0) {
+                ctx.moveTo(mousecoord[0], mousecoord[1]);
+            } else {
+                ctx.moveTo(coordlastframe[0], coordlastframe[1]);
+            }
+            ctx.lineTo(mousecoord[0], mousecoord[1]);
+
+            // START OF // LINE WIDTHS // LINE WIDTHS // LINE WIDTHS
+            // pen pressure drops or ink runs down, either way, pressure falls each frame, Min value 1.5
+            penpressure = Math.max(penpressure - 0.05, 1.5);
+            // mouse movement vector length per frame. Using it to determine line size. Faster = thinner line
+            let vectorlen = Math.hypot(
+                Math.abs(mousecoord[0] - coordlastframe[0]),
+                Math.abs(mousecoord[1] - coordlastframe[1])
+            );
+            // 1.4142... is just a number that came up often while moving really slow with my mouse
+            // seemed like a good zero state in this case but could be anything, just a value to clamp lowest values to
+            let bigwidth =
+                3 -
+                Math.min(Math.max(vectorlen - 1.4142135623730951, 0), 6) / 2;
+            // smooths size change per frame 1/4 on the new pressure & penpressure :
+            bigwidth = Math.min((bigwidth + lastbigwidth * 3) / 4, penpressure);
+            let smallwidth =
+                1.75 -
+                Math.min(Math.max(vectorlen - 1.4142135623730951, 0), 2) / 2;
+            // smooths size change per frame 1/3 on the new pressure & penpressure :
+            smallwidth = Math.min(
+                (smallwidth + lastsmallwidth * 2) / 3,
+                penpressure
+            );
+            lastbigwidth = bigwidth;
+            lastsmallwidth = smallwidth;
+            // END OF // LINE WIDTHS // LINE WIDTHS // LINE WIDTHS
+
+            // Royal blue shades by vector length
+            let shadeselect = Math.min(
+                Math.round(vectorlen - 2),
+                ballpointshades.length - 1
+            );
+
+            ctx.strokeStyle = ballpointshades[shadeselect];
+            ctx.lineCap = "round";
+            ctx.lineWidth = bigwidth;
+            ctx.stroke();
+            ctx.beginPath();
+            if (coordlastframe[0] === 0 && coordlastframe[1] === 0) {
+                ctx.moveTo(mousecoord[0], mousecoord[1]);
+            } else {
+                ctx.moveTo(coordlastframe[0], coordlastframe[1]);
+            }
+            ctx.lineTo(mousecoord[0], mousecoord[1]);
+            ctx.lineWidth = smallwidth;
+            ctx.strokeStyle = ballpointshades[Math.max(shadeselect - 2, 0)];
+            ctx.lineCap = "round";
+            ctx.stroke();
+            coordlastframe = [mousecoord[0], mousecoord[1]];
+        } else {
+            toggle = false;
+        }
+    };
+})();
